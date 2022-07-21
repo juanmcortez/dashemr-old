486d2e1bb29bbac16cd4a964df22fbdd893fd458
Juan M. Cortéz <juanm.cortez@gmail.com>
2022-07-21 13:41:35 -0300

    🔧 Patient demographics struct created.

---
+++ app/Http/Controllers/Patients/DemographicController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Patients;
+
+use App\Http\Controllers\Controller;
+use App\Models\Patients\Demographic;
+use App\Http\Requests\Patients\StoreDemographicRequest;
+use App\Http\Requests\Patients\UpdateDemographicRequest;
+
+class DemographicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Patients\StoreDemographicRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDemographicRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Patients\Demographic  $demographic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Demographic $demographic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Patients\Demographic  $demographic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Demographic $demographic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Patients\UpdateDemographicRequest  $request
+     * @param  \App\Models\Patients\Demographic  $demographic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDemographicRequest $request, Demographic $demographic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Patients\Demographic  $demographic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Demographic $demographic)
+    {
+        //
+    }
+}
